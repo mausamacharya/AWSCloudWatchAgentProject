cb8d29dc5f33396b775bf8c640e2ac5aa062b344
Mausam Acharya <mausamacharya99@gmail.com>
2024-05-28 09:43:11 +0530

    Update httpdservice.sh

--- httpdservice.sh
+++ httpdservice.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+#################
+## Version = 1
+## Date = 28th-May-2024
+#################
+
+# check the status of httpd.service and save the output in a variable called status
+
+status=$(systemctl is-active httpd.service)
+
+# print the output on the screen
+
+echo $status >> /home/ec2-user/httpdservice.log
